7175bfcd129c96a5c896728fa27dbd21b43da4fb
Blake <crazylikeafish@hotmail.com>
2016-08-12 18:10:45 +1000

    iterated on dice roller, added rolling to combat tracer

--- Scripts/Dice.js
+++ Scripts/Dice.js
@@ -1,55 +1,47 @@
-function Rolld4(amount){
-	var count = 0;
-	for (i = amount; i--; i >0) {
-		count += parseInt(Math.random() * 4 + 1);
-	}
-	return count;
-}
-
-function Rolld6(amount){
-	var count = 0;
-	for (i = amount; i--; i >0) {
-		count += parseInt(Math.random() * 6 + 1);
-	}
-	return count;
-}
-
-function Rolld8(amount){
-	var count = 0;
-	for (i = amount; i--; i >0) {
-		count += parseInt(Math.random() * 8 + 1);
-	}
-	return count;
-}
-
-function Rolld10(amount){
-	var count = 0;
-	for (i = amount; i--; i >0) {
-		count += parseInt(Math.random() * 10 + 1);
-	}
-	return count;
-}
-
-function Rolld12(amount){
-	var count = 0;
-	for (i = amount; i--; i >0) {
-		count += parseInt(Math.random() * 12 + 1);
-	}
-	return count;
-}
-
-function Rolld20(amount){
-	var count = 0;
-	for (i = amount; i--; i >0) {
-		count += parseInt(Math.random() * 20 + 1);
-	}
-	return count;
-}
-
-function Rolld100(amount){
-	var count = 0;
-	for (i = amount; i--; i >0) {
-		count += parseInt(Math.random() * 100 + 1);
+function RollDice(amount, dice){
+	switch (dice) {
+		case "d4":
+			var count = 0;
+			for (i = amount; i--; i >0) {
+				count += parseInt(Math.random() * 4 + 1);
+			}
+			break;
+		case "d6":
+			var count = 0;
+			for (i = amount; i--; i >0) {
+				count += parseInt(Math.random() * 6 + 1);
+			}
+			break;
+		case "d8":
+			var count = 0;
+			for (i = amount; i--; i >0) {
+				count += parseInt(Math.random() * 8 + 1);
+			}
+			break;
+		case "d10":
+			var count = 0;
+			for (i = amount; i--; i >0) {
+				count += parseInt(Math.random() * 10 + 1);
+			}
+			break;
+		case "d12":
+			var count = 0;
+			for (i = amount; i--; i >0) {
+				count += parseInt(Math.random() * 12 + 1);
+			}
+			break;
+		case "d20":
+			var count = 0;
+			for (i = amount; i--; i >0) {
+				count += parseInt(Math.random() * 20 + 1);
+			}
+			break;
+		case "d100":
+			var count = 0;
+			for (i = amount; i--; i >0) {
+				count += parseInt(Math.random() * 100 + 1);
+			}
+			break;
 	}
 	return count;
 }
