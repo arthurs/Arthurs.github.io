1d3bb850537e37476a1cb009c4b1c9a2eeb040bd
Blake <crazylikeafish@hotmail.com>
2016-08-12 17:51:40 +1000

    added dice rolling script

---
+++ Scripts/Dice.js
@@ -0,0 +1,55 @@
+function Rolld4(amount){
+	var count = 0;
+	for (i = amount; i--; i >0) {
+		count += parseInt(Math.random() * 4 + 1);
+	}
+	return count;
+}
+
+function Rolld6(amount){
+	var count = 0;
+	for (i = amount; i--; i >0) {
+		count += parseInt(Math.random() * 6 + 1);
+	}
+	return count;
+}
+
+function Rolld8(amount){
+	var count = 0;
+	for (i = amount; i--; i >0) {
+		count += parseInt(Math.random() * 8 + 1);
+	}
+	return count;
+}
+
+function Rolld10(amount){
+	var count = 0;
+	for (i = amount; i--; i >0) {
+		count += parseInt(Math.random() * 10 + 1);
+	}
+	return count;
+}
+
+function Rolld12(amount){
+	var count = 0;
+	for (i = amount; i--; i >0) {
+		count += parseInt(Math.random() * 12 + 1);
+	}
+	return count;
+}
+
+function Rolld20(amount){
+	var count = 0;
+	for (i = amount; i--; i >0) {
+		count += parseInt(Math.random() * 20 + 1);
+	}
+	return count;
+}
+
+function Rolld100(amount){
+	var count = 0;
+	for (i = amount; i--; i >0) {
+		count += parseInt(Math.random() * 100 + 1);
+	}
+	return count;
+}
